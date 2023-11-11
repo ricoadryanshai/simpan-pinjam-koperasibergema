e2e895212c88e2ee5f7ee5ac1c78a801c2d03c42
ricoadryanshai <ricoadryanshai@gmail.com>
2023-11-11 20:43:54 +0700

    membuat komponen transaksi kas

---
+++ src/client/components/TransaksiKas.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function TransaksiKas() {
+  return (
+    <>
+      <Card>
+        <h1>Card Testing</h1>
+      </Card>
+    </>
+  );
+}
--- src/pages/Transaksi.jsx
+++ src/pages/Transaksi.jsx
@@ -1,9 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Tab, Tabs } from "react-bootstrap";
-import TransaksiMasuk from "../client/components/TransaksiMasuk";
-import TransaksiKeluar from "../client/components/TransaksiKeluar";
+import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import TransaksiTable from "../client/components/TransaksiTable";
+import TransaksiKas from "../client/components/TransaksiKas";
 
 export default function Transaksi() {
   return (
@@ -14,14 +13,21 @@ export default function Transaksi() {
         fill
         style={{ marginTop: "6rem" }}
       >
-        <Tab eventKey="key-1" title="Data Transaksi">
-          <TransaksiTable />
+        <Tab eventKey="key-1" title="Data Transaksi Kas">
+          <Container fluid style={{ marginBottom: "6rem" }}>
+            <TransaksiTable />
+          </Container>
         </Tab>
-        <Tab eventKey="key-2" title="Transaksi Keluar">
-          <TransaksiMasuk />
-        </Tab>
-        <Tab eventKey="key-3" title="Transaksi Keluar">
-          <TransaksiKeluar />
+        <Tab eventKey="key-2" title="Transaksi Kas">
+          <Container fluid style={{ marginBottom: "6rem" }}>
+            <Row>
+              <Col />
+              <Col>
+                <TransaksiKas />
+              </Col>
+              <Col />
+            </Row>
+          </Container>
         </Tab>
       </Tabs>
     </>
