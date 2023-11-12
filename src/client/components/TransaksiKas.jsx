abed34215be77df03aa932204174276242928147
ricoadryanshai <ricoadryanshai@gmail.com>
2023-11-12 18:08:40 +0700

    menampilkan data dari database table transaksi di komponen transaksi kas

--- src/client/components/TransaksiKas.jsx
+++ src/client/components/TransaksiKas.jsx
@@ -1,13 +1,85 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card, Container, Table } from "react-bootstrap";
 
-export default function TransaksiKas() {
+export default function TransaksiKas(props) {
+  const { dataTransaksi } = props;
+
   return (
     <>
       <Card>
-        <h1>Card Testing</h1>
+        <Container>
+          <h4 className="mt-2 text-uppercase fw-bold">Data Transaksi Kas</h4>
+          <hr className="mt-2 mb-2" />
+          <Button className="mb-2">Tambah Transaksi</Button>
+          <Table hover responsive size="sm">
+            <thead className="table-light">
+              <tr className="text-center table-info">
+                <th>No.</th>
+                <th>Tanggal Transaksi</th>
+                <th>Jenis Transaksi</th>
+                <th>Uraian</th>
+                <th>Nominal Transaksi</th>
+                <th colSpan={2}>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataTransaksi.map((transaksi, index) => (
+                <tr className="text-center align-middle" key={index}>
+                  <td>{index + 1}</td>
+                  <td>{formatDate(transaksi.tanggalTransaksi)}</td>
+                  <td>{transaksi.jenisTransaksi}</td>
+                  <td>{transaksi.keterangan}</td>
+                  <td className="text-end">
+                    {formatRupiah(parseFloat(transaksi.nominalTransaksi))}
+                  </td>
+                  <td>
+                    <Button variant="warning">Edit</Button>
+                  </td>
+                  <td>
+                    <Button variant="danger">Delete</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
       </Card>
     </>
   );
+  function formatDate(inputDate) {
+    // Memisahkan tahun, bulan, dan tanggal dari string input
+    const [year, month, day] = inputDate.split("-");
+
+    // Membuat objek Date dengan format yang diterima oleh Date constructor (YYYY, MM, DD)
+    const formattedDate = new Date(year, month - 1, day);
+
+    // Mendapatkan tanggal, bulan, dan tahun dengan metode Date
+    const dayFormatted = formattedDate.getDate();
+    const monthFormatted = formattedDate.getMonth() + 1; // Ingat: bulan dimulai dari 0
+    const yearFormatted = formattedDate.getFullYear();
+
+    // Menggabungkan kembali dalam format "dd-mm-yyyy"
+    const result = `${dayFormatted < 10 ? "0" : ""}${dayFormatted}-${
+      monthFormatted < 10 ? "0" : ""
+    }${monthFormatted}-${yearFormatted}`;
+
+    return result;
+  }
+
+  function formatRupiah(angka) {
+    if (typeof angka !== "number") {
+      return "Rp 0,00";
+    }
+
+    // Format angka dengan koma sebagai pemisah ribuan dan dua digit desimal
+    const formattedAngka = angka.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 2,
+    });
+
+    return formattedAngka;
+  }
 }
--- src/pages/Transaksi.jsx
+++ src/pages/Transaksi.jsx
@@ -1,35 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
-import TransaksiTable from "../client/components/TransaksiTable";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import TransaksiKas from "../client/components/TransaksiKas";
+import { getTransaksi } from "../client/utils/api";
 
 export default function Transaksi() {
+  const [transaksi, setTransaksi] = useState([]);
+
+  useEffect(() => {
+    getTransaksi().then((res) => {
+      setTransaksi(res);
+    });
+  }, []);
   return (
     <>
-      <Tabs
-        defaultActiveKey="key-1"
-        className="mb-3"
-        fill
-        style={{ marginTop: "6rem" }}
-      >
-        <Tab eventKey="key-1" title="Data Transaksi Kas">
-          <Container fluid style={{ marginBottom: "6rem" }}>
-            <TransaksiTable />
-          </Container>
-        </Tab>
-        <Tab eventKey="key-2" title="Transaksi Kas">
-          <Container fluid style={{ marginBottom: "6rem" }}>
-            <Row>
-              <Col />
-              <Col>
-                <TransaksiKas />
-              </Col>
-              <Col />
-            </Row>
-          </Container>
-        </Tab>
-      </Tabs>
+      <Container fluid style={{ marginTop: "6rem", marginBottom: "6rem" }}>
+        <Row>
+          <Col />
+          <Col sm={7}>
+            <TransaksiKas dataTransaksi={transaksi} />
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </>
   );
 }
